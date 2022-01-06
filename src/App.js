35928c8a6b7b21af60eaf6784f877ea4bf13cf94
ifelipesilva <feehlipeeh422@gmail.com>
2022-01-06 11:30:13 -0300

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
+import './app.scss';
 
 function App() {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+
+        <div className="others">oi</div>
+      </div>
     </div>
   );
 }
