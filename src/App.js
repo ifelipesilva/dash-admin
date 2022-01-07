070258e3f00312070afbbf49a59d24b430461ea7
ifelipesilva <feehlipeeh422@gmail.com>
2022-01-07 10:53:00 -0300

    product page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import UserList from './pages/userList/UserList';
 import User from './pages/user/User';
 import NewUser from './pages/newUser/NewUser';
 import ProductList from './pages/productList/ProductList';
+import Product from './pages/product/Product';
 
 function App() {
   return (
@@ -20,7 +21,7 @@ function App() {
           <Route path="/user/:userId" element={<User />} />
           <Route path="/newUser" element={<NewUser />} />
           <Route path="/products" element={<ProductList />} />
-          <Route path="/products/:productsId" element={<User />} />
+          <Route path="/product/:productId" element={<Product />} />
           <Route path="/newProducts" element={<NewUser />} />
         </Routes>
       </div>
